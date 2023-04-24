f14b63444bc2006d963e7dcd95998592b81d1374
Peter Tran <ptran@pivotal.io>
2023-04-24 15:32:03 -0400

    Updates to next generation hello message

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	if target == "" {
 		target = "world"
 	}
-	fmt.Fprintf(w, "Hello 22 %s!\n", Reverse(target))
+	fmt.Fprintf(w, "Hello next generation %s!\n", Reverse(target))
 }
 
 func main() {
